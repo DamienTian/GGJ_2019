a1cd05f598bd5ed5e3c7b33d8116be7d18780656
Hao Tian <tian4damien@gmail.com>
2019-01-26 09:49:38 -0500

    Moving 3D

---
+++ GGJ2019Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    //private Rigidbody rb;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Debug.Log("MoveHorizontal" + moveHorizontal);
+        Debug.Log("MoveVertical" + moveVertical);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        transform.position += movement * speed;
+    }
+}
